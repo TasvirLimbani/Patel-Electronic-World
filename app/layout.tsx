82479de1b41b27c48bd026f61fca4486cf2914f0
Tasvir Limbani <120560952+TasvirLimbani@users.noreply.github.com>
2026-09-16 16:32:46 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/tlogo.png',
         media: '(prefers-color-scheme: light)',
       },
       {
@@ -17,11 +17,11 @@ export const metadata: Metadata = {
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/tlogo.png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/tlogo.png',
   },
 }
 
